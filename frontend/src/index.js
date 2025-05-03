d2f6bb8b538af528aad821fd51ea075c976029e9
osama <osamamohammedofficial@gmail.com>
2025-05-03 09:11:16 +0200

    render issue fixed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,18 @@
 // --------- src/index.js ---------
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './chatApp.css';
 
-ReactDOM.render(
+// 1) Grab the <div id="root"> from your public/index.html
+const container = document.getElementById('root');
+
+// 2) Tell React to manage that container
+const root = createRoot(container);
+
+// 3) Render your App
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
